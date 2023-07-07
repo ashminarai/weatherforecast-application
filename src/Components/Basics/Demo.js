81b1646e85ba9e6eaf05c4a7eaadb3a641ece81d
ashmina <raiashmina404@gmail.com>
2023-07-08 01:07:20 +0545

    commited

--- src/Components/Basics/Demo.js
+++ src/Components/Basics/Demo.js
@@ -6,11 +6,6 @@ const Demo = () => {
     This is a demo.
     my life is probably a mess because i used to love him so much and i used to pray for him but still i get hurt.
 
-    my life is probably a mess because i used to love him so much and i used to pray for him but still i get hurt.
-
-    my life is probably a mess because i used to love him so much and i used to pray for him but still i get hurt.
-
-    my life is probably a mess because i used to love him so much and i used to pray for him but still i get hurt.
     </>
   )
 }
