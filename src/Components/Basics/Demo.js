814edd56750903220f413b8f4923820a24f5bb11
ashmina <raiashmina404@gmail.com>
2023-07-08 01:08:15 +0545

    commited

--- src/Components/Basics/Demo.js
+++ src/Components/Basics/Demo.js
@@ -4,8 +4,6 @@ const Demo = () => {
   return (
     <>
     This is a demo.
-    my life is probably a mess because i used to love him so much and i used to pray for him but still i get hurt.
-
     </>
   )
 }
