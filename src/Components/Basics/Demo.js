621e496f296364627325f827717458bfa392e43a
ashmina <raiashmina404@gmail.com>
2023-07-11 23:07:06 +0545

    updated

--- src/Components/Basics/Demo.js
+++ src/Components/Basics/Demo.js
@@ -1,11 +1,19 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./Style.css";
 
 const Demo = () => {
   const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
 
-
-
+  useEffect(()=>{
+    const fetchApi= async() =>{
+      const url = `http://api.openweatherapp.org/data/2.5/weather?q= ${search}&appid=b14425a6554d189a2d7dc18a8e7d7263`
+      const response = await fetch(url);
+      const resJson = await response.json();
+      setCity(resJson);
+    };
+    fetchApi();
+  })
   return (
     <>
       <div className="box">
@@ -20,8 +28,8 @@ const Demo = () => {
 
       <div className="info">
         <h2 className="location">
-          <i class="fas fa-street-view"></i>
-          {city}
+          <i className="fas fa-street-view"></i>
+          {city.name}
         </h2>
         <h1 className="temp"></h1>
         <h3 className="tempmin_max">Min: 5.25° Cel | Max: 5.25° Cel</h3>
