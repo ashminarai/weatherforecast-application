5f79b8c94c8de22c143305a018938c86bd0d33e1
ashmina <raiashmina404@gmail.com>
2023-07-11 23:28:36 +0545

    updated

--- src/Components/Basics/Demo.js
+++ src/Components/Basics/Demo.js
@@ -1,19 +1,19 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Style.css";
 
 const Demo = () => {
   const [city, setCity] = useState("");
   const [search, setSearch] = useState("");
 
-  useEffect(()=>{
-    const fetchApi= async() =>{
-      const url = `http://api.openweatherapp.org/data/2.5/weather?q= ${search}&appid=b14425a6554d189a2d7dc18a8e7d7263`
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweatherapp.org/data/2.5/weather?q= ${search}&appid=b14425a6554d189a2d7dc18a8e7d7263`;
       const response = await fetch(url);
       const resJson = await response.json();
       setCity(resJson);
     };
     fetchApi();
-  }, [setSearch])
+  }, [search]);
   return (
     <>
       <div className="box">
@@ -21,25 +21,34 @@ const Demo = () => {
           <input
             type="search"
             className="inputFeild"
-            onChange={(event) => {setSearch(event.target.value)}}
+            onChange={(event) => {
+              setSearch(event.target.value)
+            }}
           ></input>
         </div>
-      
 
-      <div className="info">
-        <h2 className="location">
-          <i className="fas fa-street-view"></i>
-          {search}
-          {/* {city.name} */}
-        </h2>
-        <h1 className="temp"></h1>
-        <h3 className="tempmin_max">Min: 5.25° Cel | Max: 5.25° Cel</h3>
-      </div>
+        {/* using terminatory operator */}
 
-
-      <div className="wave -one"></div>
-      <div className="wave -two"></div>
-      <div className="wave -three"></div>
+        {!city ? (
+          <p>No Data Found</p>
+        ) : (
+          <div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fas fa-street-view"></i>
+                {search}
+              </h2>
+              <h1 className="temp">
+                {/* city, main, temperature savaiko data linu yo lekheko */}
+                {city.main.temp}
+              </h1>
+              <h3 className="tempmin_max">Min: 5.25° Cel | Max: 5.25° Cel</h3>
+            </div>
+          </div>
+        )}
+         <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
       </div>
     </>
   );
