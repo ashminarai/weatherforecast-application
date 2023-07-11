e85017687ae5ccca377e7c5270eab1bcc79aca59
ashmina <raiashmina404@gmail.com>
2023-07-11 22:18:20 +0545

    updated

--- src/Components/Basics/Demo.js
+++ src/Components/Basics/Demo.js
@@ -12,14 +12,22 @@ const Demo = () => {
             onChange={(event) => {}}
           ></input>
         </div>
-      </div>
+      
 
       <div className="info">
         <h2 className="location">
           <i class="fas fa-street-view"></i>
+          Pune
         </h2>
+        <h1 className="temp"></h1>
+        <h3 className="tempmin_max">Min: 5.25° Cel | Max: 5.25° Cel</h3>
       </div>
 
+
+      <div className="wave -one"></div>
+      <div className="wave -two"></div>
+      <div className="wave -three"></div>
+      </div>
     </>
   );
 };
