b8d8b90319e778e8ce5c87bbf12b05376afd8456
ashmina <raiashmina404@gmail.com>
2023-07-14 20:10:41 +0545

    updated

--- src/Components/Basics/Demo.js
+++ src/Components/Basics/Demo.js
@@ -8,7 +8,8 @@ const Demo = () => {
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = `http://api.openweatherapp.org/data/2.5/weather?q=${search})_units=metric_appid=b14425a6554d189a2d7dc18a8e7d7263`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=b14425a6554d189a2d7dc18a8e7d7263`
+      // const url = `http://api.openweatherapp.org/data/2.5/weather?q=${search})_units=metric_appid=b14425a6554d189a2d7dc18a8e7d7263`;
       const response = await fetch(url);
       const resJson = await response.json();
       setCity(resJson.main);
